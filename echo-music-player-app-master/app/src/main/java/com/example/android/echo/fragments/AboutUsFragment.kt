cebb9e9091f90160e9a86bbfada41401e54d79bb
Hozaifa <hozaifa.wahid.786@gmail.com>
2020-04-29 19:42:46 +0530

    music player app

---
+++ echo-music-player-app-master/app/src/main/java/com/example/android/echo/fragments/AboutUsFragment.kt
@@ -0,0 +1,27 @@
+package com.example.android.echo.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android.echo.R
+
+
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class AboutUsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_about_us, container, false)
+    }
+
+
+}
